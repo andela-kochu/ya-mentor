0d84dfe8b0e443dd9e2975425de30080970403b9
Kingsley Ochu <kingsley.ochu@andela.co>
2015-09-05 13:24:16 +0100

    ready

--- public/modules/mentees/services/mentees.client.service.js
+++ public/modules/mentees/services/mentees.client.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // mentees service used for communicating with the mentees REST endpoint
-angular.module('mentees').factory('mentees', ['$resource',
+angular.module('mentees').factory('Mentees', ['$resource',
 	function($resource) {
 		return $resource('mentees', {}, {
 			update: {
